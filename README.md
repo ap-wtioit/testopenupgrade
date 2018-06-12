b2f858b515ad21a95e86d231c24b29799fab3199
Andreas Perhab <andreas.perhab@wt-io-it.at>
2018-06-12 11:30:34 +0200

    create virtualenv for version 11 with python3

---
+++ README.md
@@ -0,0 +1,14 @@
+# Testopenupgrade
+
+Script to test if a Odoo module is able to be upgraded with OpenUpgrade
+
+## Prerequesites
+
+## Running
+
+To test upgrading a module just call
+
+```
+testopenupgrade.sh {module}
+```
+
--- testopenupgrade.sh
+++ testopenupgrade.sh
@@ -6,45 +6,70 @@ database_name="oodo_$timestamp"
 database_user=$USER
 branch_dir="$(pwd)/upgrades/openupgrade_$timestamp"
 #versions[0]=8.0
-#versions[1]=9.0
+versions[1]=9.0
 versions[2]=10.0
 versions[3]=11.0
+
+python_version[8]=python2
+python_version[9]=python2
+python_version[10]=python2
+python_version[11]=python3
 
 #directory to your odoo git repository
 ODOO_DIR=../odoo
 #local instalation of the OpenUpgrade repository
 LOCAL_OPENUPGRADE=../OpenUpgrade
 #modules to test the migration
-MODULES=base
+MODULES=${1:-base}
 REMOTE=origin
+
+if [ ! -e upgrades ] ; then mkdir upgrades; fi
 
 #preparing openupgrade for local stuff
 LOCAL_OPENUPGRADE_REPO="file://$(pwd)/$LOCAL_OPENUPGRADE"
 sed -i "s/'url': 'git:\/\/github.com\/OpenUpgrade\/OpenUpgrade.git',/'url': '${LOCAL_OPENUPGRADE_REPO//\//\\/}',/g" $LOCAL_OPENUPGRADE/scripts/migrate.py
 
+install_version=$(echo ${versions[@]} | awk '{print $1}')
+to_version=$(echo ${versions[@]} | awk '{print $NF}')
+echo "Testing upgrading module(s) $MODULES from $install_version to $to_version"
+
 for version in ${versions[@]}; do
+    major_version=${version:0:-2}
     echo "Version $version $database_name"
     if psql -ltq | awk -F '\|' '{print $1}' | grep $database_name > /dev/null; then
         echo "Upgrading to version $version"
         cd $LOCAL_OPENUPGRADE
-        pwd
         git pull
-        pip install -r requirements.txt
-        scripts/migrate.py -D $database_name -R $version -C $ODOO_CONFIG -B $branch_dir
-        if [ "$?" == "0" ] && ! grep -R ERROR $branch_dir/migration.log 2>&1 ; then
+        if [ ! -e $BASEDIR/venv$version ]; then
+            virtualenv --python=${python_version[$major_version]} $BASEDIR/venv$version
+            source $BASEDIR/venv$version/bin/activate
+            pip install -r requirements.txt
+            # fix image.py jpg error: https://stackoverflow.com/questions/8915296/python-image-library-fails-with-message-decoder-jpeg-not-available-pil
+            #pip install --no-cache-dir -I pillow
+        else
+            source $BASEDIR/venv$version/bin/activate
+        fi
+        # force running this script with the python version installed in the virtualenv
+        python scripts/migrate.py -D $database_name -R $version -C $ODOO_CONFIG -B $branch_dir
+        result=$?
+        echo $result
+        if [ "$result" == "0" ] && ! grep -R ERROR $branch_dir/migration.log 2>&1 ; then
             echo "Upgrade OK"
-            database_name="${database_name}_migrated"
+            #rename _migrated to _${version}
+            psql postgres $USER -c "ALTER DATABASE ${database_name}_migrated RENAME TO ${database_name}_${major_version}"
+            data_dir=$(grep -R data_dir $ODOO_CONFIG | awk '{print $3}')
+            ln -s $data_dir/filestore/${database_name} $data_dir/filestore/${database_name}_${major_version}
+            database_name="${database_name}_${major_version}"
             echo "Running Tests"
-            if [ -e $branch_dir/$version/server/openerp-server ] ; then
-                ODOO_BIN=$branch_dir/$version/server/openerp-server
+            cd $BASEDIR
+            cd $ODOO_DIR
+            git checkout $version
+            if [ -e openerp-server ] ; then
+                ODOO_BIN=./openerp-server
             else
-                ODOO_BIN=$branch_dir/$version/server/odoo-bin
+                ODOO_BIN=./odoo-bin
             fi
-            cp $ODOO_CONFIG $branch_dir/$version/odoo.cfg
-            new_addonspath=$(grep -R addons_path $branch_dir/$version/server.cfg)
-            sed 's/^addons_path = .*$/${new_addonspath}/' $branch_dir/$version/odoo.cfg
-            echo "root_path = $branch_dir/$version/server" >> $branch_dir/$version/odoo.cfg
-            $ODOO_BIN -d $database_name --db_user $USER -u $MODULES --stop-after-init --config $branch_dir/$version/odoo.cfg --test-enable --log-level=test
+            $ODOO_BIN -d $database_name --db_user $USER -u $MODULES --stop-after-init --config $ODOO_CONFIG --test-enable --log-level=test
             result=$?
             if [ "$result" != "0" ] ; then
                 echo "Tests failed after upgrade" 1>&2
@@ -63,8 +88,9 @@ for version in ${versions[@]}; do
             git checkout $REMOTE/$version
             git checkout -b $version
         fi
+        git clean -fd
         if [ ! -e $BASEDIR/venv$version ]; then
-            virtualenv --python=python2 $BASEDIR/venv$version
+            virtualenv --python=${python_version[$major_version]} $BASEDIR/venv$version
             source $BASEDIR/venv$version/bin/activate
             pip install -r requirements.txt
             # fix image.py jpg error: https://stackoverflow.com/questions/8915296/python-image-library-fails-with-message-decoder-jpeg-not-available-pil
@@ -72,17 +98,15 @@ for version in ${versions[@]}; do
         else
             source $BASEDIR/venv$version/bin/activate
         fi
-        ODOO_CONFIG="$(pwd)/odoo.conf"
+        ODOO_CONFIG="$BASEDIR/odoo.conf"
         if [ -e openerp-server ] ; then
             ODOO_BIN=./openerp-server
         else
             ODOO_BIN=./odoo-bin
         fi
-        $ODOO_BIN -d $database_name --db_user $USER -i $MODULES --stop-after-init --save
+        $ODOO_BIN -d $database_name --db_user $USER -i $MODULES --stop-after-init --save --config $ODOO_CONFIG
         if [ "$?" == "0" ] ; then
             echo "Install OK, running post install tests"
-            # odoo 9.0 loves to generate this config file (which then takes precedence over the given config!)
-            if [ -e ~/.openerp_serverrc ] ; then rm ~/.openerp_serverrc; fi
             $ODOO_BIN -d $database_name --db_user $USER -u $MODULES --stop-after-init --config $ODOO_CONFIG --test-enable --log-level=test
             result=$?
             if [ "$result" != "0" ] ; then
